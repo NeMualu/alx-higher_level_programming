39f4971cf24dafd4a3c4f67ff2b170fff40e43d7
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2023-12-08 06:39:51 -0800

    (ʘ‿ʘ) ( ͡°⊖ ͡°)

---
+++ 0-square_matrix_simple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    '''
+    Computes the square value of all integers in a matrix.
+
+    Args:
+        matrix: A 2D array of integers.
+
+    Returns:
+        A new matrix with the same size as the input matrix, where each
+        value is the square of the corresponding value in the input matrix.
+        The input matrix is not modified.
+    '''
+    new_matrix = matrix.copy()
+
+    return [[x**2 for x in row] for row in matrix]
