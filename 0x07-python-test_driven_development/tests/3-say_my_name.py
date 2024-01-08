d70e3a053459fd97713dadfa7871c1ba7e0d76fc
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2024-01-08 21:39:19 +0000

    My first commit

--- 0x07-python-test_driven_development/tests/3-say_my_name.py
+++ 0x07-python-test_driven_development/tests/3-say_my_name.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
-"""Module for a function that prints a full name."""
+say_my_name = __import__('3-say_my_name').say_my_name
 
-
-def say_my_name(first_name, last_name=""):
-    """Display a full name.
-
-    Args:
-        first_name (str): The given first name.
-        last_name (str, optional): The given last name. Defaults to an empty string.
-
-    Raises:
-        TypeError: If first_name or last_name are not string types.
-    """
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
-
+say_my_name("John", "Smith")
+say_my_name("Walter", "White")
+say_my_name("Bob")
+try:
+    say_my_name(12, "White")
+except Exception as e:
+    print(e)
