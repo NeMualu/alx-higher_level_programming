c3a2002dbbdf6ba61ce750ac4c7ee556103bd8be
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2024-03-13 23:53:03 +0000

    My first commit

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
+/**
+ * Script to print "JavaScript is amazing"
+ * 
+ * - Creates a constant variable called myVar with the value "JavaScript is amazing"
+ * - Prints the value of myVar using console.log()
+ */
 const myVar = 'Javascript is amazing';
 console.log(myVar);
