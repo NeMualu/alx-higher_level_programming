3b6842f9cba80583b1f27f5cdf84898cdc8bc698
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2024-03-28 13:23:21 -0700

    My first commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-"""Write a Python script that fetches https://intranet.hbtn.io/status"""
+"""
+Fetches https://alx-intranet.hbtn.io/status using urllib
+"""
+
 import urllib.request
 
-if __name__ == '__main__':
+def main():
+    url = 'https://alx-intranet.hbtn.io/status'
 
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read()
-        print('Body response:')
-        print("\t- type: {}".format(type(the_page)))
-        print("\t- content: {}".format(the_page))
-        print("\t- utf8 content: {}".format(the_page.decode('utf-8')))
+    with urllib.request.urlopen(url) as response:
+        html_content = response.read()
+        utf8_content = html_content.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type:", type(html_content))
+    print("\t- content:", html_content)
+    print("\t- utf8 content:", utf8_content)
+
+if __name__ == "__main__":
+    main()
+
