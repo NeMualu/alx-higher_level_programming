dbd74eec2215cc014264fc9a8f5871f8d62dc4c8
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2024-03-31 13:33:10 -0700

    My first commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 # This Bash script takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-# Send a request to the specified URL and display the size of the body of the response in bytes
 curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
 
