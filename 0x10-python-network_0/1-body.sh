89296ee90db69e5be5d0098ba60f578d21eb2b57
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2024-03-31 13:55:58 -0700

    My first commit

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This Bash script takes in a URL, sends a GET request to the URL, and displays the body of the response
-# Using curl to send a GET request to the provided URL and following redirections
-curl -sL "$1" -o /dev/null -w "%{http_code}"
+# Using curl to send a GET request to the provided URL and following redirections, then outputting the HTTP status code
+curl -sIL "$1" | grep -E "^HTTP" | tail -n1 | awk '{print $2}'
