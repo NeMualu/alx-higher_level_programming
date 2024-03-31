19da8b483f01598e720c6e6ba0f9b5dbfcde5a3e
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2024-03-31 13:39:21 -0700

    My first commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This Bash script takes in a URL, sends a GET request to that URL, and displays the size of the body of the response
-
 curl -sI "$1" | awk '/Content-Length/ {print $2}'
