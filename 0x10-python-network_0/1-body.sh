3426428b0e0744639fe6be2501eddfa4ca89c786
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2024-03-31 13:52:26 -0700

    My first commit

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 # This Bash script takes in a URL, sends a GET request to the URL, and displays the body of the response
-response=$(curl -sIL "$1" -o /dev/null -w "%{http_code}")
-if [ "$response" = '200' ]; then
-    echo "no redirection"
-elif [ "$response" = '301' ]; then
-    echo "1 redirection"
-else
-    echo "5 redirection with multiple status code"
-fi
+# Using curl to send a GET request to the provided URL and following redirections
+curl -sL "$1" -o /dev/null -w "%{http_code}"
