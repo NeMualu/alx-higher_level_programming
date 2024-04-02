9a8ad690990b77aacb9f9f7904dc56d65f2d4a75
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2024-04-01 20:19:43 -0700

    My first commit

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This Bash script sends a DELETE request to the URL passed as the first argument and displays the body of the response
+# This Bash script sends a DELETE request to the URL passed as the first argument and displays the body of the re
 curl -s "$1" -X DELETE
