6b50c371209ae45374805025068b3a756d85291e
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2024-04-01 20:34:28 -0700

    My first commit

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
 # This Bash script sends a request to a URL passed as an argument and displays only the status code of the response.
-
-# Send a request to the URL and output only the status code
-curl_status=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-
-# Display the status code
-echo "$curl_status"
-
+curl -s -o /dev/null -w "%{http_code}" "$1" 
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
 # This Bash script makes a request to the specified URL with a JSON payload and causes the server to respond with a message containing "You got me!" in the body of the response.
-
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <URL> <JSON_FILE>"
-    exit 1
-fi
-
-# Send a POST request with a JSON payload to the specified URL
 curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
-
